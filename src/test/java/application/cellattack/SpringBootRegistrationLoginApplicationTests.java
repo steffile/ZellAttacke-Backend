0198b1f4adce64755bc72b9d4da59d40c2c58d8a
steffile <s31315@bht-berlin.de>
2022-01-26 17:14:33 +0100

    HTML-Dateien angepasst

---
+++ src/test/java/application/cellattack/SpringBootRegistrationLoginApplicationTests.java
@@ -0,0 +1,17 @@
+package application.cellattack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @source https://www.codejava.net/frameworks/spring-boot/user-registration-and-login-tutorial 
+ *
+ */
+@SpringBootTest
+class SpringBootRegistrationLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
